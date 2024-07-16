eb66fa559d9b9c048fd99cda49b14189c8a531de
MarioFelipe19 <felipeparra257@hotmail.com>
2024-07-16 17:10:05 -0500

    feat: :sparkles: creacion de ejemplos de js
    se cream ejemplos de codigo js
    
    By @MarioParra

---
+++ Temas/Dia2/Dia2.js
@@ -0,0 +1,43 @@
+//switch
+let mes = 11;
+switch (mes) {
+  case 1:
+    console.log("Enero");
+    break;
+  case 2:
+    console.log("Febrero");
+    break;
+  case 3:
+    console.log("Marzo");
+    break;
+  case 4:
+    console.log("Abril");
+    break;
+  case 5:
+    console.log("Mayo");
+    break;
+  case 6:
+    console.log("junio");
+    break;
+  case 7:
+    console.log("julio");
+    break;
+  case 8:
+    console.log("agosto");
+    break;
+  case 9:
+    console.log("septiembre");
+    break;
+  case 10:
+    console.log("octubre");
+    break;
+  case 11:
+    console.log("noviembre");
+    break;
+  case 12:
+    console.log("diciembre");
+    break;
+  default:
+    console.log("Se acabaron los meses");
+    break;
+}
