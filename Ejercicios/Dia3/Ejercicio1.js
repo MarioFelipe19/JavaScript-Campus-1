157d4d21713cefc8f81348689d069a7e71491e39
MarioFelipe19 <felipeparra257@hotmail.com>
2024-07-18 11:43:15 -0500

    feat: :sparkles: se crea archivos de ejercicios
    se crean archivos para realizar la tarea de clase
    
    By @MarioParra

---
+++ Ejercicios/Dia3/Ejercicio1.js
@@ -0,0 +1,6 @@
+console.log("Buscar la palabra ingresada en la cadena guardada")
+
+function buscar(){
+    let frase = "Hola como estan, aqui desde colombia para el mundo";
+    let palabra = new 
+}
