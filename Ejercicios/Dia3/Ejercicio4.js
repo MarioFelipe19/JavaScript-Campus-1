738a131e24c6a7d9bb7220f90d055dd3ffe3c77f
MarioFelipe19 <felipeparra257@hotmail.com>
2024-07-19 21:17:04 -0500

    feat: :sparkles: se agrega informacion a ejercicios 4 y 5
    se coloca los comentarios a los ejercicios 4 y 5  para su entendimiento detallado
    
    By @MarioParra

--- Ejercicios/Dia3/Ejercicio4.js
+++ Ejercicios/Dia3/Ejercicio4.js
@@ -1,10 +1,10 @@
-console.log("Buscar la palabra ingresada en la cadena guardada")
+console.log("Buscar el patron de coincidencia en una cadena dada")
 
-let regex = /(\d+)\s(\w+)/g;
-let listaCompras = "Compré 10 manzanas y 5 naranjas";
+let regex = /(\d+)\s(\w+)/g; // esta linea busca los patrones de un numero despues de una palabra
+let listaCompras = "Compré 10 manzanas y 5 naranjas"; // esta linea contiene la cadena a la que se le buscara los patrones 
 
-let resultado = listaCompras.matchAll(regex);
+let resultado = listaCompras.matchAll(regex);// esta linea devuelve un arreglo  con los patrones de la cadena ingrasada 
 
-for (let match of resultado) {
+for (let match of resultado) { // esta liena tiene un bucle for el cual  recorre y itera sobre las coincidencias para imprimirlas 
   console.log(match);
 }
